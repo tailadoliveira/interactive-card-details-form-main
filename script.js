451592ea2b01a7ad82d287ccea3aa8d28646952c
Taila De Oliveira <tailadoliveira@gmail.com>
2023-03-30 17:03:07 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,23 @@
+function enviardados() {
+    alert('enviou')
+}
+
+function inputName(value) {
+    document.getElementById('name-card').innerHTML = value
+}
+
+function inputCardNum(value) {
+    document.getElementById('number-card').innerHTML = value
+}
+
+function inputMM(value) {
+    document.getElementById('expiry-month-card').innerHTML = value
+}
+
+function inputYY(value) {
+    document.getElementById('expiry-year-card').innerHTML = value
+}
+
+function inputCvc(value) {
+    document.getElementById('cvc-display-card').innerHTML = value
+}
